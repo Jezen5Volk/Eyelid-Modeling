5708f6772fa63b7512f8b58094a1274bd09c0839
Samantha Herman <138075884+Jezen5Volk@users.noreply.github.com>
2025-03-09 15:52:17 -0700

    training overhead created
    Made helper functions for training, validating, and testing once a model, optimizer, and loss function have been selected

--- models/RNN_rect.py
+++ models/RNN_rect.py
@@ -2,8 +2,12 @@ import torch
 
 class EMG_RNN(torch.nn.Module):
 
-    def __init__(self, input_size, hidden_size, num_layers, dropout):
+    def __init__(self, input_size, H1, N1, dropout):
         super().__init__()
+        self.BN1 = torch.nn.BatchNorm1d(input_size)
+        self.RNN1 = torch.nn.RNN(input_size, H1, num_layers = N1, batch_first = True, nonlinearity = 'relu', dropout = dropout)
         
-
-
+    def forward(self, X):
+        X = self.BN1(X)
+        X = self.RNN1(X)
+        return X
---
+++ utils/training_overhead.py
@@ -0,0 +1,78 @@
+import torch
+
+class Trainer:
+    def __init__(self, train_dl, val_dl, model, loss_fn, optimizer, batch_size, epochs):
+        self.model = model
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+        self.train_dl = train_dl
+        self.val_dl = val_dl
+        self.batch_size = batch_size
+        self.epochs = epochs
+
+    
+    def train(self, verbose = False):
+        self.verbose = verbose
+        for t in range(self.epochs):
+            print(f"Epoch {t+1}\n-------------------------------")
+            self.train_loop()
+            self.val_loop()
+        print("Done!")
+
+        return self.model.state_dict()
+
+
+    def train_loop(self):
+        size = len(self.train_dl.dataset)
+        self.model.train()
+
+        for batch, (X, y) in enumerate(self.train_dl):
+            pred = self.model(X)
+            loss = self.loss_fn(pred, y)
+
+            #backpropagation
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+
+            if batch % 100 == 0 and self.verbose: 
+                loss, current = loss.item(), batch * self.batch_size + len(X)
+                print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+    def val_loop(self):
+        self.model.eval()
+        num_batches = len(self.val_dl)
+
+        test_loss = 0
+        err = 0
+
+        with torch.no_grad():
+            for X, y in self.val_dl:
+                pred = self.model(X)
+                test_loss += self.loss_fn(pred, y)
+                test_acc = torch.max((pred-y)/y)*100
+                err = torch.max((test_acc, err))
+        
+        test_loss /= num_batches
+        
+        print(f"Validation Error: \n Max Marker Error: {(err):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+    
+    def test_model(self, test_dl):
+        self.model.eval()
+        num_batches = len(test_dl)
+
+        test_loss = 0
+        err = 0
+
+        with torch.no_grad():
+            for X, y in test_dl:
+                pred = self.model(X)
+                test_loss += self.loss_fn(pred, y)
+                test_acc = torch.max((pred-y)/y)*100
+                err = torch.max((test_acc, err))
+        
+        test_loss /= num_batches
+        
+        print(f"Test Error: \n Max Marker Error: {(err):>0.1f}%, Avg loss: {test_loss:>8f} \n")
