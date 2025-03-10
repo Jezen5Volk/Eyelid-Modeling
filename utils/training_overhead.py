446254b1c04a8dcfcb0567f247ddaa74729ae418
Samantha Herman <138075884+Jezen5Volk@users.noreply.github.com>
2025-03-10 13:59:46 -0700

    two_norm3D implemented

--- utils/training_overhead.py
+++ utils/training_overhead.py
@@ -48,19 +48,20 @@ class Trainer:
         self.model.eval()
         num_batches = len(self.val_dl)
 
-        test_loss = 0
+        val_loss = 0
         err = 0
 
         with torch.no_grad():
             for X, y in self.val_dl:
                 pred = self.model(X)
-                test_loss += self.loss_fn(pred, y)
-                test_acc = torch.max((pred-y)/(y+1e-8))
-                err = torch.max(torch.Tensor([test_acc, err]))
+                val_loss += self.loss_fn(pred, y)
+                val_acc = self.two_norm3D(pred, y)
+                err = torch.max(torch.Tensor([val_acc, err]))
+                
         
-        test_loss /= num_batches
+        val_loss /= num_batches
         
-        print(f"Validation Error: \n Max Marker Error: {(err):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+        print(f"Validation Error: \n Max Marker Error: {(err):>0.1f}%, Avg loss: {val_loss:>8f} \n")
 
     
     def test_model(self, test_dl):
@@ -74,9 +75,36 @@ class Trainer:
             for X, y in test_dl:
                 pred = self.model(X)
                 test_loss += self.loss_fn(pred, y)
-                test_acc = torch.max((pred-y)/y)*100
-                err = torch.max((test_acc, err))
+
+                test_acc = self.two_norm3D(pred, y)
+                err = torch.max(torch.Tensor([test_acc, err]))
         
         test_loss /= num_batches
         
         print(f"Test Error: \n Max Marker Error: {(err):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+    
+    def two_norm3D(self, pred, y, eps = 1e-8):
+        X_pred = pred[:,:,0,:]
+        X = y[:,:,0,:]
+        X_diff = X_pred - X
+
+        Y_pred = pred[:,:,1,:]
+        Y = y[:,:, 1, :]
+        Y_diff = Y_pred - Y
+
+        Z_pred = pred[:,:,2,:]
+        Z = y[:,:, 2, :]
+        Z_diff = Z_pred - Z
+
+        magnitude = torch.sqrt(X**2 + Y**2 + Z**2)
+        diff = torch.sqrt(X_diff**2 + Y_diff**2 + Z_diff**2)
+
+        err = diff/(magnitude + eps)*100
+        max_err = torch.max(err)
+
+        return max_err
+
+
+
+
