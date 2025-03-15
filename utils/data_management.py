fd3fd98e3c696aeceb9a5f9d4ac8947359c144af
Samantha Herman <138075884+Jezen5Volk@users.noreply.github.com>
2025-03-14 17:34:06 -0700

    ready 2 run experiments

--- utils/data_management.py
+++ utils/data_management.py
@@ -242,8 +242,8 @@ class Preprocessor:
         yr = self.yr
         Xr = self.Xr
 
-        num_win = int((t_y - t_lookahead*yr)//int(t_stride*yr))
-    
+        num_win = int((t_y - t_lookahead*yr)/(t_stride*yr))
+        
         X_win = np.empty((int(t_win*Xr), N, num_win, C_x))
         y_win = np.empty((int(t_stride*yr), d, N, num_win, C_y))
         init_state = np.empty((int(t_stride*yr), d, N, C_y))
